394c8fc61adff4f080925bc05d11675008bb4dc0
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-06-30 10:42:19 +0100

    fix: change logo image to lowercase

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,11 @@
 // import React from "react";
-import logo from "../../assets/images/logo.png";
+import logoing from "../../assets/images/logo.png";
 
 const Logo = () => {
   return (
     <div className="flex items-center gap-6 md:ml-3 mlg:w-[275px] mlg:justify-between mlg:ml-5">
       <div>
-        <img src={logo} alt="LEARNEXO" className="w-full h-full" />
+        <img src={logoimg} alt="LEARNEXO" className="w-full h-full" />
       </div>
       <span className="font-inter text-blue-3 font-bold text-4xl">
         LearNEXO
