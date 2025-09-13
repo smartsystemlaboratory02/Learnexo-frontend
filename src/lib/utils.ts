d9ed265c5229ff014e72622c2ad6ffe32715320a
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-09-13 15:53:56 +0100

    chore: updates

---
+++ src/lib/utils.ts
@@ -0,0 +1,31 @@
+import { config } from "@/components/config";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// eslint-disable-line @typescript-eslint/no-unused-vars
+type GroupBy<T> = Record<string, T[]>;
+
+export function groupBy<T, K extends keyof T>(array: T[], key: K): GroupBy<T> {
+  return array.reduce((acc, item) => {
+    const keyValue = String(item[key]);
+    if (!acc[keyValue]) {
+      acc[keyValue] = [];
+    }
+    acc[keyValue].push(item);
+    return acc;
+  }, {} as GroupBy<T>);
+}
+
+export function absoluteUrl(path: string) {
+  return process.env.NODE_ENV === "development"
+    ? `http://localhost:3000${path}`
+    : `https://${config.appUrl}${path}`;
+}
